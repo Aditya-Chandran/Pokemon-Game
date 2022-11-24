73d7d9821bc372f553b1ddc9aaffe2a434c97a52
SuyeshJadhav <suyesh.j@somaiya.edu>
2022-11-24 21:28:56 +0530

    adds pokemon and moves indices

--- static_database/Moves.java
+++ static_database/Moves.java
@@ -33,90 +33,90 @@ public class Moves {
             /*3*/Moves Heat_Wave =new Moves("Heat Wave",1,95,90,20);
             moveList.add(Heat_Wave);
 
-            /*1*/Moves Magma_Storm =new Moves("Magma Storm",1,100,75,30);
+            /*4*/Moves Magma_Storm =new Moves("Magma Storm",1,100,75,30);
             moveList.add(Magma_Storm);
 
-            Moves Fusion_Flare =new Moves("Fusion Flare",1,100,95,45);
+            /*5*/Moves Fusion_Flare =new Moves("Fusion Flare",1,100,95,45);
             moveList.add(Fusion_Flare);
 
-            Moves Eruption =new Moves("Eruption",1,150,100,65);
+            /*6*/Moves Eruption =new Moves("Eruption",1,150,100,65);
             moveList.add(Eruption);
 
 
 
             // ************************             WATER TYPE            *************************
-            Moves Water_Gun=new Moves("Water Gun",2,40,100,0);
+            /*7*/Moves Water_Gun=new Moves("Water Gun",2,40,100,0);
             moveList.add(Water_Gun);
 
-            Moves Bubble=new Moves("Bubble",2,40,100,0);
+            /*8*/Moves Bubble=new Moves("Bubble",2,40,100,0);
             moveList.add(Bubble);
 
-            Moves Water_Pulse=new Moves("Water Pulse",2,60,100,6);
+            /*9*/Moves Water_Pulse=new Moves("Water Pulse",2,60,100,6);
             moveList.add(Water_Pulse);
 
-            Moves Waterfall=new Moves("Waterfall",2,40,80,13);
+            /*10*/Moves Waterfall=new Moves("Waterfall",2,40,80,13);
             moveList.add(Waterfall);
 
-            Moves Muddy_Water=new Moves("Muddy Water",2,90,85,21);
+            /*11*/Moves Muddy_Water=new Moves("Muddy Water",2,90,85,21);
             moveList.add(Muddy_Water);
 
-            Moves Surf=new Moves("Surf",2,90,100,32);
+            /*12*/Moves Surf=new Moves("Surf",2,90,100,32);
             moveList.add(Surf);
 
-            Moves Hydro_Pump=new Moves("Hydro Pump",2,110,80,45);
+            /*13*/Moves Hydro_Pump=new Moves("Hydro Pump",2,110,80,45);
             moveList.add(Hydro_Pump);
 
-            Moves Hydro_Cannon=new Moves("Hydro Cannon",2,150,90,60);
+            /*14*/Moves Hydro_Cannon=new Moves("Hydro Cannon",2,150,90,60);
             moveList.add(Hydro_Cannon);
 
-            Moves Water_Sprout=new Moves("Water Sprout",2,150,100,80);
+            /*15*/Moves Water_Sprout=new Moves("Water Sprout",2,150,100,80);
             moveList.add(Water_Sprout);
 
         
 
             // ************************             GRASS TYPE            *************************
-            Moves Vine_Whip=new Moves("Vine Whip",3,40,100,0);
+            /*16*/Moves Vine_Whip=new Moves("Vine Whip",3,40,100,0);
             moveList.add(Vine_Whip);
             
-            Moves Razor_Leaf=new Moves("Razor leaf",3,55,95,5);
+            /*17*/Moves Razor_Leaf=new Moves("Razor leaf",3,55,95,5);
             moveList.add(Razor_Leaf);
             
-            Moves Giga_Drain=new Moves("Giga Drain",3,75,100,15);
+            /*18*/Moves Giga_Drain=new Moves("Giga Drain",3,75,100,15);
             moveList.add(Giga_Drain);
 
-            Moves Leaf_Blade=new Moves("Leaf Blade",3,90,100,30);
+            /*19*/Moves Leaf_Blade=new Moves("Leaf Blade",3,90,100,30);
             moveList.add(Leaf_Blade);
 
-            Moves Petal_Blizzard=new Moves("Petal Blizzard",3,90,100,48);
+            /*20*/Moves Petal_Blizzard=new Moves("Petal Blizzard",3,90,100,48);
             moveList.add(Petal_Blizzard);
 
-            Moves Solar_Beam=new Moves("Solar Beam",3,120,100,65);
+            /*21*/Moves Solar_Beam=new Moves("Solar Beam",3,120,100,65);
             moveList.add(Solar_Beam);
 
-            Moves Solar_Blade=new Moves("Solar Blade",3,120,100,85);
+            /*22*/Moves Solar_Blade=new Moves("Solar Blade",3,120,100,85);
             moveList.add(Solar_Blade);
 
             // ************************             ELECTRIC TYPE            *************************
-            Moves Thundershock=new Moves("Thundershock",4,46,100,0);
+            /*23*/Moves Thundershock=new Moves("Thundershock",4,46,100,0);
             moveList.add(Thundershock);
             
-            Moves ThunderBolt=new Moves("ThunderBolt",4,90,100,15);
+            /*24*/Moves ThunderBolt=new Moves("ThunderBolt",4,90,100,15);
             moveList.add(ThunderBolt);
 
-            Moves Volt_Tackle=new Moves("Volt_Tackle",4,120,100,40);
+            /*25*/Moves Volt_Tackle=new Moves("Volt_Tackle",4,120,100,40);
             moveList.add(Volt_Tackle);
 
             // ************************             GROUND TYPE            *************************
-            Moves bonemerang=new Moves("Bonemerang",2,50,90,32);
+            /*26*/Moves bonemerang=new Moves("Bonemerang",2,50,90,32);
             moveList.add(bonemerang);
 
-            Moves earthquake=new Moves("Earthquake",2,100,100,32);
+            /*27*/Moves earthquake=new Moves("Earthquake",2,100,100,32);
             moveList.add(earthquake);
             
-            Moves dig=new Moves("Dig",2,100,100,32);
+            /*28*/Moves dig=new Moves("Dig",2,100,100,32);
             moveList.add(dig);
             
-            Moves fissure=new Moves("Fissure",2,999999,30,32);
+            /*29*/Moves fissure=new Moves("Fissure",2,999999,30,32);
             moveList.add(fissure);
 
             // ************************             PSYCHIC TYPE            *************************
@@ -127,7 +127,5 @@ public class Moves {
             // ************************             DARK TYPE            *************************
             // ************************             DRAGON TYPE            *************************
             // ************************             GHOST TYPE            *************************
-
-
         }
 }
--- static_database/Pokemon.java
+++ static_database/Pokemon.java
@@ -37,165 +37,165 @@ public class Pokemon {
         /*2*/Pokemon growlithe=new Pokemon("Growlithe",1,60);
         pokList.add(growlithe);
         
-        Pokemon ponyta=new Pokemon("Ponyta",1,90);
+        /*3*/Pokemon ponyta=new Pokemon("Ponyta",1,90);
         pokList.add(ponyta);
 
-        Pokemon magmar=new Pokemon("Magmar",1,93);
+        /*4*/Pokemon magmar=new Pokemon("Magmar",1,93);
         pokList.add(magmar);
 
-        Pokemon cyndaquil=new Pokemon("Cyndaquil",1,65);
+        /*5*/Pokemon cyndaquil=new Pokemon("Cyndaquil",1,65);
         pokList.add(cyndaquil);
 
-        Pokemon torchic=new Pokemon("Torchic",1,45);
+        /*6*/Pokemon torchic=new Pokemon("Torchic",1,45);
         pokList.add(torchic);
 
         
         // ************************             WATER TYPE            *************************
 
 
-        Pokemon squirtle=new Pokemon("Squirtle",2,43);
+        /*7*/Pokemon squirtle=new Pokemon("Squirtle",2,43);
         pokList.add(squirtle);
         
-        Pokemon psyduck=new Pokemon("Psyduck",2,55);
+        /*8*/Pokemon psyduck=new Pokemon("Psyduck",2,55);
         pokList.add(psyduck);
         
-        Pokemon krabby=new Pokemon("Krabby",2,50);
+        /*9*/Pokemon krabby=new Pokemon("Krabby",2,50);
         pokList.add(krabby);
 
-        Pokemon totodile=new Pokemon("Totodile",2,43);
+        /*10*/Pokemon totodile=new Pokemon("Totodile",2,43);
         pokList.add(totodile);
 
-        Pokemon mudkip=new Pokemon("Mudkip",2,40);
+        /*11*/Pokemon mudkip=new Pokemon("Mudkip",2,40);
         pokList.add(mudkip);
         
         // ************************             GRASS TYPE            *************************
 
 
-        Pokemon bulbasaur=new Pokemon("Bulbasaur",3,45);
+        /*12*/Pokemon bulbasaur=new Pokemon("Bulbasaur",3,45);
         pokList.add(bulbasaur);
 
-        Pokemon chikorita=new Pokemon("Chikorita",3,45);
+        /*13*/Pokemon chikorita=new Pokemon("Chikorita",3,45);
         pokList.add(chikorita);
 
-        Pokemon treecko=new Pokemon("Treecko",3,70);
+        /*14*/Pokemon treecko=new Pokemon("Treecko",3,70);
         pokList.add(treecko);
         
         // ************************             FLYING TYPE            *************************
 
 
-        Pokemon pidgey=new Pokemon("Pidgey",4,56);
+        /*15*/Pokemon pidgey=new Pokemon("Pidgey",4,56);
         pokList.add(pidgey);
 
-        Pokemon starly=new Pokemon("Starly",4,60);
+        /*16*/Pokemon starly=new Pokemon("Starly",4,60);
         pokList.add(starly);
 
         // ************************             ELECTRIC TYPE            *************************
         
 
-        Pokemon pikachu=new Pokemon("Pikachu",5,90);
+        /*17*/Pokemon pikachu=new Pokemon("Pikachu",5,90);
         pokList.add(pikachu);
 
-        Pokemon electabuzz=new Pokemon("Electabuzz",5,105);
+        /*18*/Pokemon electabuzz=new Pokemon("Electabuzz",5,105);
         pokList.add(electabuzz);
 
-        Pokemon electrike=new Pokemon("Electrike",5,65);
+        /*19*/Pokemon electrike=new Pokemon("Electrike",5,65);
         pokList.add(electrike);
 
 
         // ************************             GROUND TYPE            *************************
         
 
-        Pokemon sandshrew=new Pokemon("Sandshrew",6,40);
+        /*20*/Pokemon sandshrew=new Pokemon("Sandshrew",6,40);
         pokList.add(sandshrew);
         
-        Pokemon diglet=new Pokemon("Diglet",6,95);
+        /*21*/Pokemon diglet=new Pokemon("Diglet",6,95);
         pokList.add(diglet);
 
-        Pokemon cubone=new Pokemon("Cubone",6,35);
+        /*22*/Pokemon cubone=new Pokemon("Cubone",6,35);
         pokList.add(cubone);
         
         // ************************             PSYCHIC TYPE            *************************
 
 
-        Pokemon abra=new Pokemon("Abra",7,90);
+        /*23*/Pokemon abra=new Pokemon("Abra",7,90);
         pokList.add(abra);
         
-        Pokemon ralts=new Pokemon("Ralts",7,40);
+        /*24*/Pokemon ralts=new Pokemon("Ralts",7,40);
         pokList.add(ralts);
 
         
         // ************************             FIGHTING TYPE            *************************
 
 
-        Pokemon machop=new Pokemon("Machop",8,35);
+        /*25*/Pokemon machop=new Pokemon("Machop",8,35);
         pokList.add(machop);
 
-        Pokemon makuhita=new Pokemon("Makuhita",8,25);
+        /*26*/Pokemon makuhita=new Pokemon("Makuhita",8,25);
         pokList.add(makuhita);
         
-        Pokemon timburr=new Pokemon("Timburr",8,35  );
+        /*27*/Pokemon timburr=new Pokemon("Timburr",8,35  );
         pokList.add(timburr);
         
         // ************************             POISON TYPE            *************************
 
 
-        Pokemon grimer=new Pokemon("Grimer",9,25);
+        /*28*/Pokemon grimer=new Pokemon("Grimer",9,25);
         pokList.add(grimer);
         
         
         // ************************             NORMAL TYPE            *************************
         
         
-        Pokemon tauros=new Pokemon("Tauros",10,110);
+        /*29*/Pokemon tauros=new Pokemon("Tauros",10,110);
         pokList.add(tauros);
 
-        Pokemon snorlax=new Pokemon("Snorlax",10,30);
+        /*30*/Pokemon snorlax=new Pokemon("Snorlax",10,30);
         pokList.add(snorlax);
 
-        Pokemon porygon2=new Pokemon("Porygon2",10,60);
+        /*31*/Pokemon porygon2=new Pokemon("Porygon2",10,60);
         pokList.add(porygon2);
     
 
         // ************************             ROCK TYPE            *************************
 
 
-        Pokemon sudowoodo=new Pokemon("Sudowoodo",11,30);
+        /*32*/Pokemon sudowoodo=new Pokemon("Sudowoodo",11,30);
         pokList.add(sudowoodo);
 
-        Pokemon boldore=new Pokemon("Boldore",11,20);
+        /*33*/Pokemon boldore=new Pokemon("Boldore",11,20);
         pokList.add(boldore);
         
         
-        // ************************             DARK TYPE            *************************
+        //  ***********************             DARK TYPE            *************************
 
 
-        Pokemon poochyena=new Pokemon("Poochyena",12,35);
+        /*34*/Pokemon poochyena=new Pokemon("Poochyena",12,35);
         pokList.add(poochyena);
 
-        Pokemon absol=new Pokemon("Absol",12,75);
+        /*35*/Pokemon absol=new Pokemon("Absol",12,75);
         pokList.add(absol);
 
 
         // ************************             DRAGON TYPE            *************************
 
 
-        Pokemon bagon=new Pokemon("Bagon",13,50);
+        /*36*/Pokemon bagon=new Pokemon("Bagon",13,50);
         pokList.add(bagon);
 
-        Pokemon dratini=new Pokemon("Dratini",13,50);
+        /*37*/Pokemon dratini=new Pokemon("Dratini",13,50);
         pokList.add(dratini);
         
-        Pokemon axew=new Pokemon("Axew",13,57);
+        /*38*/Pokemon axew=new Pokemon("Axew",13,57);
         pokList.add(axew);
 
 
         // ************************             GHOST TYPE            *************************
 
 
-        Pokemon ghastly=new Pokemon("Ghastly",14,80);
+        /*39*/Pokemon ghastly=new Pokemon("Ghastly",14,80);
         pokList.add(ghastly);
         
-        Pokemon duskull=new Pokemon("Duskull",14,20);
+        /*40*/Pokemon duskull=new Pokemon("Duskull",14,20);
         pokList.add(duskull);
 
     }
